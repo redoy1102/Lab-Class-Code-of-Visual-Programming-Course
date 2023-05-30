4864f8bf135bb4160a528259ca71757cd7bb5721
Md Fuadul Islam Redoy <mdfuadulislamhridoy52@gmail.com>
2023-05-30 22:30:14 +0600

    Code added

--- Theory Class/Enum/Enum/Program.cs
+++ Theory Class/Enum/Enum/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace Enum
 {
-    internal class Program
+    
+    internal class Program:enumWithSwitch
     {
+        enum weekDays
+        {
+            Sunday = 1,
+            Monday,
+            Tuesday = 'F'
+        }
         static void Main(string[] args)
         {
+            //Printing the enums
+            weekDays w = weekDays.Sunday;
+            Console.WriteLine(w);
+
+            //Printing the index numbers
+            int a = (int)weekDays.Sunday;
+            int b = (int)weekDays.Monday;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            //printing character values
+            char c = (char)weekDays.Tuesday;
+            Console.WriteLine(c);
+
+            //enum with switch
+            monthName mn = monthName.January;
+            switch (mn)
+            {
+                case monthName.January:
+                    Console.WriteLine("January");
+                    break;
+                case monthName.February:
+                    Console.WriteLine("February");
+                    break;
+            }
+                
+
         }
     }
 }
---
+++ Theory Class/Enum/Enum/enumWithSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    enum monthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    internal class enumWithSwitch
+    {
+
+    }
+}
